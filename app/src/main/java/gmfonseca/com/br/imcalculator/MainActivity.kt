724a17ad3a84ae0cf56d2ed4431bf978b569ea16
gmfonseca <gmagnofonseca@gmail.com>
2019-06-07 13:51:09 -0300

    Adicionado classificação de IMC e APP Icon.

--- app/src/main/java/gmfonseca/com/br/imcalculator/MainActivity.kt
+++ app/src/main/java/gmfonseca/com/br/imcalculator/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private var calcButton: Button? = null
 
+    private var imcClass: TextView? = null
     private var imcText: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,21 +28,22 @@ class MainActivity : AppCompatActivity() {
         calcButton?.setOnClickListener {
 
             val imc = calculateIMC(java.lang.Double.parseDouble("${alturaInput?.text}"), java.lang.Double.parseDouble("${pesoInput?.text}"))
+            val classificacao = imcClassification(imc)
 
             if(imc != -1.0) {
                 val df = DecimalFormat("#.##")
+                imcClass?.setText("${classificacao}")
                 imcText?.setText("IMC: ${df.format(imc)}")
             }
             else
                 Toast.makeText(applicationContext, "Informe um valor válido.", Toast.LENGTH_SHORT).show()
         }
-
-
     }
 
     private fun initComponents(){
         alturaInput = findViewById(R.id.alturaInput)
         pesoInput = findViewById(R.id.pesoInput)
+        imcClass = findViewById(R.id.imcClass)
         imcText = findViewById(R.id.imcResult)
         calcButton = findViewById(R.id.botao)
     }
@@ -55,5 +57,20 @@ class MainActivity : AppCompatActivity() {
             imc = weight / ((height/100).pow(2))
 
         return imc
+    }
+
+    private fun imcClassification(imc: Double) : String{
+        val classification: String
+
+        if(imc < 16) classification = "Magreza grave"
+        else if(16 < imc && imc < 17) classification = "Magreza moderada"
+        else if(17 < imc && imc < 18.5) classification = "Magreza Leve"
+        else if(18.5 < imc && imc < 25) classification = "Saudável"
+        else if(25 < imc && imc < 30) classification = "Sobrepeso"
+        else if(30 < imc && imc < 35) classification = "Obesidade Grau I"
+        else if(35 < imc && imc < 40) classification = "Obesidade Grau II (severa)"
+        else classification = "Obesidade Grau III (mórbida)"
+
+        return classification
     }
 }
